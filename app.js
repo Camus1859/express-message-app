cf12846fd60accb00f154d379cf90f7f5c852799
Anderson <laventurepsu1@gmail.com>
2021-05-16 03:28:54 -0400

    updated app.js to accept request object

--- app.js
+++ app.js
@@ -4,35 +4,40 @@ const PORT = process.env.PORT || 5000;
 const indexRouter = require('./routes/index')
 const newRouter = require('./routes/new')
 const app = express();
+const path = require('path');
+
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use('/', indexRouter)
-app.use('/', newRouter)
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(indexRouter)
+app.use(newRouter)
 
 
 
-
+// app.use(express.static(path.join(__dirname, 'public')));
 
 
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
+// app.use(function(req, res, next) {
+//   next(createError(404));
+// });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  console.log(err.message)
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+// // error handler
+// app.use(function(err, req, res, next) {
+//   // set locals, only providing error in development
+//   console.log(err.message)
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render('error');
+// });
 
 
 
--- routes/new.js
+++ routes/new.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const messages = '../../messages'
+const messages = require('../messages')
 
 router.post('/new', (req, res) => {
+  console.log(req.body)
   const newMessage = {
     text: req.body.text,
     user: req.body.name,
     added: new Date(),
   };
   messages.push(newMessage)
+  res.redirect('/')
 });
 
 module.exports = router;
