9cda0471662cb0a4c69f7e0dd4024a255a9c3d47
Anderson <laventurepsu1@gmail.com>
2021-05-16 00:52:38 -0400

    added error handling

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 const express = require('express');
 const exphbs = require('express-handlebars');
-const app = express();
 const PORT = process.env.PORT || 5000;
+const indexRouter = require('./routes/index')
+const app = express();
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
+
+app.use('/', indexRouter)
+
+
+
+
+
+
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  console.log(err.message)
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port http://localhost:${PORT}`);
