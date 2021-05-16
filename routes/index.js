d563de2211498f8524f0e8e49b517db527dc2402
Anderson <laventurepsu1@gmail.com>
2021-05-16 01:01:08 -0400

    added routers

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,8 @@
 const express= require('express')
 const router= express.Router()
 
+router.get('/',(req, res, next)=>{
+  res.render('index',{ title: 'Mini-Message-App'})
+})
+
+module.exports = router
